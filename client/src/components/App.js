401e0cc24e1404caa1d5b40213b2d51e25cecac7
Shane Phillips <shane@loongphillips.com>
2022-03-30 17:22:03 -0400

    basic cart and some styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Cart from "./Cart";
+import Header from "./Header";
 import Homepage from "./Homepage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-      </Switch>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={Homepage} />
+      </Routes>
+      <Routes>
+        <Route exact path="/cart" element={Cart} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-    <div>
+    <Wrapper>
       <Link to={`/`}>
-        <h1>Header</h1>
+        <h3>Header</h3>
       </Link>
-    </div>
+      <Link to={`/cart`}>
+        <h3>Shopping Cart</h3>
+      </Link>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  height: 75px;
+  padding: 10px;
+  border: solid 2px black;
+`;
+
+export default Header;
