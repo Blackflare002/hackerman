7640f9447ba1fdf00605f7a7a31dd1a82f241d29
eric-chan-js <eric.chan.js@gmail.com>
2022-03-30 20:19:45 -0400

    addItemToCart and removeItemFromCart

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -8,7 +8,8 @@ const initialState = {
   items: null,
   cart: {
     items: [],
-    totalCost: 0
+    totalCost: 0,
+    size: 0,
   },
 };
 
@@ -35,12 +36,57 @@ const reducer = (action, state) => {
         ...state,
         items: action.items,
       };
+    case "add-item-to-cart":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: [
+            ...state.cart.items,
+            action.item, // add item to items array in cart
+          ],
+          totalCost: state.cart.totalCost + Number(item.cost.slice(1)), // add price to total
+          size: state.cart.size + 1, // increment cart size by 1
+        },
+      };
+    case "remove-item-from-cart":
+      const { items } = state.cart;
+
+      // remove one item of id provided
+      const foundIndex = items.findIndex((item) => item._id === action._id);
+      const updatedItems = [...items];
+      let priceToDeduct = 0;
+      // only remove item if provided item id is found inside cart
+      if (foundIndex !== -1) {
+        updatedItems.splice(foundIndex, 1);
+        // if item is found, find the price to deduct from totalCost
+        priceToDeduct = Number(items[foundIndex].price.slice(1));
+      }
+
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: updatedItems,
+          totalCost: state.cart.totalCost - priceToDeduct,
+          size: state.cart.size - 1,
+        },
+      };
     default:
   }
 };
 
 export const AppContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItemToCart = useCallback(
+    (item) => dispatch({ type: "add-item-to-cart", item }),
+    [dispatch]
+  );
+
+  const removeItemFromCart = useCallback((_id) =>
+    dispatch({ type: "remove-item-from-cart", _id })
+  );
 
   const setStatusIdle = useCallback(
     () => dispatch({ type: "set-status-idle" }),
@@ -53,11 +99,11 @@ export const AppContextProvider = ({ children }) => {
   );
 
   const setStatusError = useCallback((error) =>
-    dispatch({ type: "set-status-error", error })
+    dispatch({ type: "set-status-error", error }, [dispatch])
   );
 
   const getItems = useCallback((items) =>
-    dispatch({ type: "get-items", items })
+    dispatch({ type: "get-items", items }, [dispatch])
   );
 
   // fetch items from db on load
@@ -71,13 +117,15 @@ export const AppContextProvider = ({ children }) => {
       })
       .catch((err) => setStatusError(err));
   }, []);
-  
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         actions: {
+          addItemToCart,
           getItems,
+          removeItemFromCart,
           setStatusError,
           setStatusLoading,
           setStatusIdle,
