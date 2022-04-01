fc61b02efd9277e605db77ad12731421523919ac
eric-chan-js <eric.chan.js@gmail.com>
2022-04-01 10:56:42 -0400

    import font css file

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { AppContextProvider } from './components/AppContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { AppContextProvider } from "./components/AppContext";
+import "./fonts/index.css";
 
 ReactDOM.render(
   // <React.StrictMode>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>,
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>,
   // </React.StrictMode>, // strict mode causes increments/decrements in reducers to run twice
-  document.getElementById('root')
+  document.getElementById("root")
 );
