7d505ffd63bc67e366ee728708ced9c724354f9a
Your Full Name <eddy.lachapelle@hotmail.com>
2022-04-01 12:04:44 -0400

    css changes on cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "./AppContext";
+import styled from "styled-components";
 
 const Cart = () => {
   const {
@@ -13,24 +14,99 @@ const Cart = () => {
   };
 
   return (
-    <div>
-      <div>This is the shopping cart page.</div>
-      <p>{size} items in cart</p>
-      <p>Total: ${totalCost}</p>
+    <Wrapper>
+      <Title>Shopping Cart</Title>
+      <HeaderWrapper></HeaderWrapper>
+      <LineBreak></LineBreak>
+
       <ul>
         {cart &&
           items.map((item) => (
             <li>
-              <p>{item.name}</p>
-              <p>{item.price}</p>
-              <button onClick={() => handleRemoveFromCart(item._id)}>
-                Delete
-              </button>
+              <ProductInfo1>
+                <StyledImage src={item.imageSrc} />
+
+                <Info>
+                  <Information>{item.name}</Information>
+                  <Information>{item.price}</Information>
+                  <Category>{item.category}</Category>
+
+                  <StyledButton onClick={() => handleRemoveFromCart(item._id)}>
+                    Delete
+                  </StyledButton>
+                </Info>
+              </ProductInfo1>
+              <LineBreak></LineBreak>
             </li>
           ))}
+        <SubtotalWrapper>
+          <p> Subtotal ({size} items) </p>
+          <p>Total: ${totalCost}</p>
+        </SubtotalWrapper>
       </ul>
-    </div>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  margin-top: 100px;
+  border: 2px solid grey;
+  border-radius: 9px;
+  max-width: 700px;
+  box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
+    0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
 
+  margin: 0 auto;
+`;
+const Title = styled.h1`
+  font-size: 40px;
+  margin-top: 20px;
+  margin-left: 10px;
+`;
+const LineBreak = styled.div`
+  border-bottom: 1px solid grey;
+  margin-top: 10px;
+`;
+const StyledImage = styled.img`
+  border-radius: 5px;
+  width: 150px;
+`;
+const HeaderWrapper = styled.div``;
+const SubtotalWrapper = styled.div`
+  display: flex;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  justify-content: flex-end;
+  margin-right: 10px;
+`;
+const ProductInfo1 = styled.div`
+  display: flex;
+  align-items: center;
+  line-height: 35px;
+  margin-left: 10px;
+  margin-top: 8px; ;
+`;
+const Category = styled.span`
+  font-style: italic;
+  margin-left: 10px;
+`;
+const StyledButton = styled.button`
+  background-color: #ffd633;
+  color: white;
+  font-weight: bold;
+  border-radius: 7px;
+  max-width: 65px;
+  border: none;
+  margin-left: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Information = styled.span`
+  margin-left: 10px;
+`;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export default Cart;
--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -4,8 +4,6 @@ import { AppContext } from "./AppContext";
 import { colors, fontFamily } from "./GlobalStyles";
 import { Link } from "react-router-dom";
 
-
-
 import styled from "styled-components";
 
 const Item = () => {
@@ -22,21 +20,20 @@ const Item = () => {
       .then((data) => setItemData(data.singleItem))
       .catch((err) => setStatusError(err));
   }, []);
-  
+
   useEffect(() => {
-    if (itemData) { // only fetch company by ID if itemData exists
+    if (itemData) {
+      // only fetch company by ID if itemData exists
       fetch(`/companies/${itemData.companyId}`)
-      .then((res) => res.json())
-      .then((data) => setCompanyData(data.singleCompanies))
-      .catch((err) => setStatusError(err));
+        .then((res) => res.json())
+        .then((data) => setCompanyData(data.singleCompanies))
+        .catch((err) => setStatusError(err));
     }
-  }, [itemData]) // will fire useEffect when itemData gets updated
-  
+  }, [itemData]); // will fire useEffect when itemData gets updated
+
   const handleAddToCart = () => {
     addItemToCart(itemData);
   };
-
-  
 
   return (
     // rendering Items and Company info
@@ -46,35 +43,41 @@ const Item = () => {
           <ItemWrapper>
             <ItemImage>
               <Image src={itemData.imageSrc} />
-              </ItemImage>
-              <ItemInfo>
-                <ItemDescription>
-                  <ItemName>{itemData.name}</ItemName>
-                  <Category>Category: Cyber-{itemData.category}</Category>                
-                  <BodyLocation>Fan of Hackerman?<br></br>Suit-up with this perfect fit for your  {itemData.body_location}</BodyLocation>                
-                </ItemDescription>
-                <PurchaseSection>
-                  <ItemAvailability>
-                    {itemData.numInStock < 5 ?
-                    <Stock>{`Items in Stock: ${itemData.numInStock} (Hurry-up!)`} </Stock> :
+            </ItemImage>
+            <ItemInfo>
+              <ItemDescription>
+                <ItemName>{itemData.name}</ItemName>
+                <Category>Category: Cyber-{itemData.category}</Category>
+                <BodyLocation>
+                  Fan of Hackerman?<br></br>Suit-up with this perfect fit for
+                  your {itemData.body_location}
+                </BodyLocation>
+              </ItemDescription>
+              <PurchaseSection>
+                <ItemAvailability>
+                  {itemData.numInStock < 5 ? (
+                    <Stock>
+                      {`Items in Stock: ${itemData.numInStock} (Hurry-up!)`}{" "}
+                    </Stock>
+                  ) : (
                     <Stock>{`Items in Stock: ${itemData.numInStock}`} </Stock>
-                    }
-                  </ItemAvailability>
-                  <ItemPrice>
-                    <Price>{itemData.price}</Price>
-                  </ItemPrice>                
-                </PurchaseSection>
-                <CompanyInfoWrapper>
-                  <CompanyInfo to={companyData.url}>        
-                    <CompanyName>{companyData.name}</CompanyName>
-                    <Country>{companyData.country}</Country>                    
-                  </CompanyInfo>
-                  <Cartbutton>
-                    <Button onClick={handleAddToCart}>ADD TO CART</Button>
-                  </Cartbutton>
-                </CompanyInfoWrapper>
-              </ItemInfo>
-            </ItemWrapper>
+                  )}
+                </ItemAvailability>
+                <ItemPrice>
+                  <Price>{itemData.price}</Price>
+                </ItemPrice>
+              </PurchaseSection>
+              <CompanyInfoWrapper>
+                <CompanyInfo to={companyData.url}>
+                  <CompanyName>{companyData.name}</CompanyName>
+                  <Country>{companyData.country}</Country>
+                </CompanyInfo>
+                <Cartbutton>
+                  <Button onClick={handleAddToCart}>ADD TO CART</Button>
+                </Cartbutton>
+              </CompanyInfoWrapper>
+            </ItemInfo>
+          </ItemWrapper>
         </>
       )}
     </Wrapper>
@@ -84,7 +87,6 @@ const Item = () => {
 export default Item;
 
 const Wrapper = styled.div`
-
   position: absolute;
   top: 200px;
   display: flex;
@@ -93,10 +95,9 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   background-color: black;
-`
+`;
 
 const ItemWrapper = styled.div`
-
   width: 60%;
   height: 50%;
   display: flex;
@@ -107,102 +108,86 @@ const ItemWrapper = styled.div`
   padding: 30px;
   color: #fff;
   box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
-    0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;  
-`
+    0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
+`;
 
 const ItemImage = styled.div`
-  
   display: flex;
   justify-content: center;
   width: 400px;
   height: 400px;
-  padding: 10px;  
+  padding: 10px;
   align-items: center;
-`
+`;
 const Image = styled.img`
-  
   width: 350px;
   height: 350px;
   border-radius: 10px;
   box-shadow: 0 3px 10px whitesmoke;
-`
+`;
 
 const ItemInfo = styled.div`
-
   width: 75%;
   height: 300px;
-  
-`
+`;
 
 const ItemDescription = styled.div`
-
-  display: grid;  
+  display: grid;
   margin-bottom: 50px;
   align-items: center;
   gap: 20px;
-`
+`;
 const ItemName = styled.p`
-
   margin: 0px;
   font-family: ${fontFamily};
-  font-size: 32px;  
-`
+  font-size: 32px;
+`;
 
 const Category = styled.p`
-
   margin: 0px;
   font-family: ${fontFamily};
   font-size: 18px;
   font-style: italic;
-`
+`;
 const BodyLocation = styled.p`
-
   margin: 0px;
   font-family: ${fontFamily};
-`
+`;
 const Stock = styled.p`
-
   margin: 0px;
   font-family: ${fontFamily};
-  font-size: 16px;  
-`
+  font-size: 16px;
+`;
 const Price = styled.p`
-
   margin: 0px;
   font-family: ${fontFamily};
   font-size: 32px;
   text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa,
-  0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;
-`
+    0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;
+`;
 
 const PurchaseSection = styled.div`
-
   display: grid;
-  grid-template-columns: auto auto;  
+  grid-template-columns: auto auto;
   align-items: center;
   gap: 170px;
   margin-bottom: 50px;
-`
+`;
 
 const ItemAvailability = styled.div`
-
   width: 150px;
-`
+`;
 
-const ItemPrice = styled.div`
-`
+const ItemPrice = styled.div``;
 
-const Cartbutton = styled.div`  
-`
+const Cartbutton = styled.div``;
 
 const CompanyInfoWrapper = styled.div`
-
-  display: flex;  
+  display: flex;
   align-items: center;
-  gap: 150px;  
-`
+  gap: 150px;
+`;
 const CompanyInfo = styled(Link)`
-
   text-align: center;
   text-decoration: none;
   color: whitesmoke;
@@ -210,43 +195,44 @@ const CompanyInfo = styled(Link)`
   border: solid white 1px;
   padding: 10px;
   box-shadow: 0 3px 10px whitesmoke;
-  border-radius: 10px;;
-`
+  border-radius: 10px; ;
+`;
 const CompanyName = styled.p`
   margin: 0px;
   font-family: ${fontFamily};
   font-size: 20px;
-`
+`;
 const Country = styled.p`
   margin: 0px;
   font-family: ${fontFamily};
   font-style: italic;
-`
+`;
 const CompanyLink = styled(Link)`
   margin: 0px;
   font-family: ${fontFamily};
   text-decoration: none;
   color: whitesmoke;
-`
+`;
 
 const Button = styled.button`
   width: 300px;
   height: 50px;
   font-size: 16px;
-  font-family: ${fontFamily};  
+  font-family: ${fontFamily};
   color: #bc13fe;
   box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
-  0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
+    0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
   text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa,
-  0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;
-    
-  :hover {    
-    color: #0fa;
-    transition: box-shadow 0.5s ease-in-out;    
-    box-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa,
     0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;
+
+  :hover {
+    cursor: pointer;
+    color: #0fa;
+    transition: box-shadow 0.5s ease-in-out;
+    box-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa,
+      0 0 82px #0fa, 0 0 92px #0fa, 0 0 102px #0fa, 0 0 151px #0fa;
   }
   :active {
     transform: translateY(4px);
-}
-`
+  }
+`;
